77bdf25256072cfceca29a308841ac736c45e8c7
Rizaldi Martaputra <zal@fastmail.com>
2023-08-27 15:03:00 +0700

    Add layout file

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,10 @@
 module.exports = function(eleventyConfig) {
-  // Return your Object options:
-  return {
-    dir: {
-      input: "content"
-    }
-  }
+	// Return your Object options:
+	return {
+		dir: {
+			input: "content",
+			includes: "../_includes",
+			data: "../_data"
+		}
+	}
 };
